d4816015254ba51e4cdc36b396d0e517f9d84855
Kamil Cudnik <kcudnik@gmail.com>
2022-03-25 02:20:48 +0100

    [syncd] Fix comparison logic for new transferred objects (#1024)
    For created objects like buffer pool or buffer profile
    those are not on the switch, but they were treated as such
    and transferred to temporary view during comparison logic
    causing to duplicate objects on temporary view

--- tests/BCM56850.pl
+++ tests/BCM56850.pl
@@ -707,7 +707,11 @@ sub test_brcm_warm_new_object_port_serdes
 {
     fresh_start;
 
-    play "empty_sw.rec";
+    # use buffer profile and pool objects since they
+    # are not default created on the switch
+    # so asic operations should still be zero
+
+    play "buffer_profile_get_A.rec";
 
     print "port serdes objects in ASIC_DB: ";
     print `redis-cli -n 1 keys "*_SERDES*" | wc -l`;
@@ -732,7 +736,7 @@ sub test_brcm_warm_new_object_port_serdes
 
     start_syncd_warm;
 
-    play "empty_sw.rec", 0;
+    play "buffer_profile_get_A.rec", 0;
 
     print "check ASIC_DB for serdes\n";
     print "RIDTOVID: ", `redis-cli -n 1 HKEYS RIDTOVID |grep oid:0x5700 |wc -l`;
