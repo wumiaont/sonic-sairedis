ec64c3f446b833d9432e7931dae916baeabbb984
richardyu-ms <richard.yu@microsoft.com>
2022-10-19 08:11:08 +0000

    [SAI submodule update] Enable support for SAI v1.11.0
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

--- lib/ClientSai.cpp
+++ lib/ClientSai.cpp
@@ -1260,6 +1260,38 @@ sai_status_t ClientSai::bulkCreate(
             object_statuses);
 }
 
+sai_status_t ClientSai::bulkCreate(
+        _In_ uint32_t object_count,
+        _In_ const sai_neighbor_entry_t* neighbor_entry,
+        _In_ const uint32_t *attr_count,
+        _In_ const sai_attribute_t **attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    MUTEX();
+    SWSS_LOG_ENTER();
+    REDIS_CHECK_API_INITIALIZED();
+
+    // TODO support mode
+
+    std::vector<std::string> serialized_object_ids;
+
+    // on create vid is put in db by syncd
+    for (uint32_t idx = 0; idx < object_count; idx++)
+    {
+        std::string str_object_id = sai_serialize_neighbor_entry(neighbor_entry[idx]);
+        serialized_object_ids.push_back(str_object_id);
+    }
+
+    return bulkCreate(
+            SAI_OBJECT_TYPE_NEIGHBOR_ENTRY,
+            serialized_object_ids,
+            attr_count,
+            attr_list,
+            mode,
+            object_statuses);
+}
+
 // BULK CREATE HELPERS
 
 sai_status_t ClientSai::bulkCreate(
@@ -1436,6 +1468,26 @@ sai_status_t ClientSai::bulkRemove(
     return bulkRemove(SAI_OBJECT_TYPE_MY_SID_ENTRY, serializedObjectIds, mode, object_statuses);
 }
 
+sai_status_t ClientSai::bulkRemove(
+        _In_ uint32_t object_count,
+        _In_ const _sai_neighbor_entry_t *neighbor_entry,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    MUTEX();
+    SWSS_LOG_ENTER();
+    REDIS_CHECK_API_INITIALIZED();
+
+    std::vector<std::string> serializedObjectIds;
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+    {
+        serializedObjectIds.emplace_back(sai_serialize_neighbor_entry(neighbor_entry[idx]));
+    }
+
+    return bulkRemove(SAI_OBJECT_TYPE_NEIGHBOR_ENTRY, serializedObjectIds, mode, object_statuses);
+}
+
 // BULK REMOVE HELPERS
 
 sai_status_t ClientSai::bulkRemove(
@@ -1605,6 +1657,27 @@ sai_status_t ClientSai::bulkSet(
     }
 
     return bulkSet(SAI_OBJECT_TYPE_MY_SID_ENTRY, serializedObjectIds, attr_list, mode, object_statuses);
+}
+
+sai_status_t ClientSai::bulkSet(
+        _In_ uint32_t object_count,
+        _In_ const sai_neighbor_entry_t *neighbor_entry,
+        _In_ const sai_attribute_t *attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    MUTEX();
+    SWSS_LOG_ENTER();
+    REDIS_CHECK_API_INITIALIZED();
+
+    std::vector<std::string> serializedObjectIds;
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+    {
+        serializedObjectIds.emplace_back(sai_serialize_neighbor_entry(neighbor_entry[idx]));
+    }
+
+    return bulkSet(SAI_OBJECT_TYPE_NEIGHBOR_ENTRY, serializedObjectIds, attr_list, mode, object_statuses);
 }
 
 // BULK SET HELPERS
