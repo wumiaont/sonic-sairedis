bd211806f471a3156799d521164771c91b46b4a1
prabhataravind <108555774+prabhataravind@users.noreply.github.com>
2025-05-02 08:34:20 -0700

    Enable zmq mode in syncd for smartswitch DPUs (#1566)
    * Enable zmq mode in syncd for smartswitch DPUs
    
     * Enable zmq mode by default if device type in $SYNCD_VARS is SmartSwitchDPU
     * Provide a context configuration to syncd with the appropriate context keys
     * This goes along with the changes in https://github.com/sonic-net/sonic-buildimage/pull/21940
    
    Signed-off-by: Prabhat Aravind <paravind@microsoft.com>
    
    * Use switch_type instead of type to enable zmq
    
    Signed-off-by: Prabhat Aravind <paravind@microsoft.com>
    
    ---------
    
    Signed-off-by: Prabhat Aravind <paravind@microsoft.com>

--- syncd/scripts/syncd_init_common.sh
+++ syncd/scripts/syncd_init_common.sh
@@ -37,9 +37,13 @@ mkdir -p /var/log/sai_failure_dump/
 # currently disabled since most vendors don't support that yet
 # CMD_ARGS+=" -l"
 
-# Set synchronous mode if it is enabled in CONFIG_DB
+# Set zmq mode by default for smartswitch DPU
+# Otherwise, set synchronous mode if it is enabled in CONFIG_DB
 SYNC_MODE=$(echo $SYNCD_VARS | jq -r '.synchronous_mode')
-if [ "$SYNC_MODE" == "enable" ]; then
+SWITCH_TYPE=$(echo $SYNCD_VARS | jq -r '.switch_type')
+if [ "$SWITCH_TYPE" == "dpu" ]; then
+    CMD_ARGS+=" -z zmq_sync -x /usr/share/sonic/hwsku/context_config.json"
+elif [ "$SYNC_MODE" == "enable" ]; then
     CMD_ARGS+=" -s"
 fi
 
