41e4ca94b4a7d06d8da1a474156b52a2797e757c
Oleksandr Ivantsiv <oivantsiv@nvidia.com>
2025-05-02 10:24:31 -0700

    [nvidia-bluefield] Remove switchdev configuration from syncd init. (#1592)
    This configuration will be applied by a separate systemd service on system start.

--- syncd/scripts/syncd_init_common.sh
+++ syncd/scripts/syncd_init_common.sh
@@ -526,25 +526,7 @@ config_syncd_nvidia_bluefield()
     mkdir -p /mnt/huge
     mount -t hugetlbfs pagesize=1GB /mnt/huge
 
-    devlink dev eswitch set pci/0000:03:00.0 mode legacy
-    devlink dev eswitch set pci/0000:03:00.0 mode switchdev
-
-    if [[ $hwsku != *"-C1" ]] && [[ $single_port == false ]]; then
-        devlink dev param set pci/0000:03:00.0 name esw_multiport value 1 cmode runtime
-        devlink dev param set pci/0000:03:00.1 name esw_multiport value 1 cmode runtime
-    fi
-
     ethtool -A Ethernet0 rx off tx off
-
-    if [[ $single_port == false ]]; then
-        eth4_mac=$(cat /sys/class/net/Ethernet4/address)
-        echo "PORT_2_MAC_ADDRESS=$eth4_mac" >> /tmp/sai.profile
-
-        devlink dev eswitch set pci/0000:03:00.1 mode legacy
-        devlink dev eswitch set pci/0000:03:00.1 mode switchdev
-
-        ethtool -A Ethernet4 rx off tx off
-    fi
 }
 
 config_syncd_xsight()
